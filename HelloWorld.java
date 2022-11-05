4e35eacc326a296d6dd55b6571131ba25bba483b
neetikhare <neetikhare025@gmail.com>
2022-11-05 19:08:16 +0530

    commit for validate jenkins build automation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello! It's my first java project");
-		System.out.println("Welcome to CDAC NOida");
+		System.out.println("We are in develop branch");
 	}
 }
