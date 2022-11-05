c33cb668dbc492d78f818da4189dfc2c8cb8b1fc
neetikhare <neetikhare025@gmail.com>
2022-11-05 19:11:32 +0530

    validate automation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello! It's my first java project");
-		System.out.println("We are in develop branch");
+		System.out.println("We are validating automation");
 	}
 }
